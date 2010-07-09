59538cd879cebdb3d7cfaa95baef0da41604fd45
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2010-07-09 19:07:11 +0600

    Add buildout to sys.path

--- grandma/manage.py
+++ grandma/manage.py
@@ -4,7 +4,8 @@ import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path[0:0] = [
     os.path.abspath(current_dir),
-    os.path.abspath(os.path.join(current_dir, '..', 'parts', 'django'))
+    os.path.abspath(os.path.join(current_dir, '..', 'parts', 'django')),
+    os.path.abspath(os.path.join(current_dir, '..', 'parts', 'zc.buildout')),
 ]
 
 from django.core.management import execute_manager
