b50010710a27ac8f4f1482cf0fb8b72a91f7e0c3
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2010-07-19 21:54:27 +0600

    Add sitemap.xml

--- grandma/templates/grandma/project/urls.py
+++ grandma/templates/grandma/project/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls.defaults import patterns, include, handler404, handler500
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.sitemaps import GenericSitemap
 
 handler404
 handler500
@@ -10,31 +11,25 @@ handler500
 admin.autodiscover()
 urlpatterns = patterns('')
 
+sitemaps = {
+}
+
 if settings.DEBUG:
     pass
 
 urlpatterns += patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': settings.MEDIA_ROOT}),
+        {'document_root': settings.MEDIA_ROOT}),
 )
 
 urlpatterns += patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^robots.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 )
 
 #pages_dict = {
 #    'queryset': Page.objects.exclude(status=Page.DRAFT),
 #    'date_field': 'last_modification_date',
 #}
-#
-#news_dict = {
-#    'queryset': News.objects.filter(show=True),
-#    'date_field': 'date',
-#}
-#
-#sitemaps = {
 #    'pages': GenericSitemap(pages_dict),
-#    'news': GenericSitemap(news_dict),
-#}
-#    (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
